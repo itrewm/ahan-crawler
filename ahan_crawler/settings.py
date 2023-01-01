ecc22e6e65474ca4e6c1908b6c769211a7a9f3b0
itrewm <94739533+itrewm@users.noreply.github.com>
2023-01-02 03:02:27 +0330

    Update settings.py

--- ahan_crawler/settings.py
+++ ahan_crawler/settings.py
@@ -18,6 +18,10 @@ NEWSPIDER_MODULE = 'ahan_crawler.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
+## settings.py
+
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
